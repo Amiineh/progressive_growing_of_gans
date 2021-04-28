d58c4766165aa62249dfac35e69cb626fb50e12b
amineh <amineh.ahm@gmail.com>
2021-04-28 13:48:00 +0430

    aot config change

--- tfutil.py
+++ tfutil.py
@@ -12,6 +12,7 @@ import importlib
 import imp
 import numpy as np
 from collections import OrderedDict
+#import tensorflow as tf
 import tensorflow.compat.v1 as tf
 tf.disable_v2_behavior()
 
@@ -55,7 +56,7 @@ def absolute_name_scope(scope): # Forcefully enter the specified name scope, ign
 
 def init_tf(config_dict=dict()):
     if tf.compat.v1.get_default_session() is None:
-        tf.set_random_seed(np.random.randint(1 << 31))
+        tf.compat.v1.set_random_seed(np.random.randint(1 << 31))
         create_session(config_dict, force_as_default=True)
 
 #----------------------------------------------------------------------------
@@ -64,6 +65,7 @@ def init_tf(config_dict=dict()):
 
 def create_session(config_dict=dict(), force_as_default=False):
     config = tf.compat.v1.ConfigProto()
+    config.gpu_options.allow_growth = True
     for key, value in config_dict.items():
         fields = key.split('.')
         obj = config
